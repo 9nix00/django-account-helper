5fccd0630d95ddb05c7564130357ece47653ebab
Breeze.kay <wangwenpei@nextoa.com>
2016-03-18 17:26:41 +0800

    remove django package required

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='django-account-helper',
     version=account_helper.version,
     packages=find_packages(exclude=["tests"]),
-    install_requires=['django'],
+    # install_requires=['django'],
     url='https://github.com/9nix00/django-account-helper',
     license='http://opensource.org/licenses/MIT',
     download_url='https://github.com/9nix00/django-account-helper/archive/master.zip',
