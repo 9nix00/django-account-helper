fc4b5315865eec1bd4122f88952e453fa053e165
WANG WENPEI <wangwenpei@nextoa.com>
2016-07-24 07:46:52 +0800

    add session support

--- account_helper/middleware.py
+++ account_helper/middleware.py
@@ -3,6 +3,14 @@
 from threading import local
 
 _user = local()
+_session = local()
+
+class CurrentSessionMiddleware(object):
+    def process_request(self, request):
+        _session.value = request.session
+
+def get_current_session():
+    return _session.value if hasattr(_session, 'value') else {}
 
 
 class CurrentUserMiddleware(object):
